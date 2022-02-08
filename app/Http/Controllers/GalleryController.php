054edc0b914ebc2e8c0387349962911fa426df5a
Muhammad Luthfi Azzammi <luthfi_azzammi@treetan.com>
2022-02-08 19:34:34 +0700

    Successfully Show Images From S3

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -4,14 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class GalleryController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $image = Image::all();
-        dd($image);
+        $image = Image::latest()->first();
 
-        return view('gallery');
+        $filenames = json_decode($image->name);
+        $paths = json_decode($image->image_path);
+
+        $names = [];
+        $images = [];
+        for ($i=0; $i < count($paths); $i++){
+            $names[] = $filenames[$i];
+            $images[] = Storage::disk('s3')->url($paths[$i]);
+        }
+
+        $data = [
+            "names" => $names,
+            "images" => $images
+        ];
+
+        return view('gallery', $data);
     }
 }
